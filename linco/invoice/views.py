492c7c1ab9700fa93a5e0def28ed63156b154153
danialbagheri <bagheri.danial@gmail.com>
2019-06-20 12:39:30 +0100

    hides the invoice list from unautheticated users

--- linco/invoice/views.py
+++ linco/invoice/views.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from json import JSONEncoder
+import datetime
+from django.http import JsonResponse
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from invoice.forms import InvoicesForm, SupplierForm
 from django.shortcuts import render, redirect
@@ -9,188 +12,202 @@ from django.contrib import messages
 # Create your views here.
 # from django.views.generic import ListView
 # from itertools import chain
+
+
 def HomePage(request):
-	return render(request, 'index.html', context={})
+    return render(request, 'index.html', context={})
+
 
 def SearchView(request):
-	costs = 0
-    # context['query'] = self.request.GET.get('q')
-	query = request.POST.get('q', None)
-	if query is not None:
-		invoices_results = Invoices.objects.search(query)
-		qs = invoices_results
-		for i in qs:
-			costs += i.invoice_value
-		total = costs
-		return render(request, 'search.html', context={"result": qs, "total": total})
-	else:
-		qs = Invoices.objects.none()
-		return render(request, 'search.html', context={"result": qs})
+    costs = 0
+# context['query'] = self.request.GET.get('q')
+    query = request.POST.get('q', None)
+    if query is not None:
+        invoices_results = Invoices.objects.search(query)
+        qs = invoices_results
+        for i in qs:
+            costs += i.invoice_value
+        total = costs
+        return render(request, 'search.html', context={"result": qs, "total": total})
+    else:
+        qs = Invoices.objects.none()
+        return render(request, 'search.html', context={"result": qs})
+
 
 def InvoiceUpload(request):
-	if request.method == 'POST':
-		form = InvoicesForm(request.POST, request.FILES)
-		if form.is_valid():
-			form.save()
-			messages.success(request, 'Invoice has been successfully recorded')
-		else:
-			form = InvoicesForm(request.POST)
-			messages.error(request, 'Unfortunately your form submission was unsuccessful, please try again')
-	else:
-		form = InvoicesForm()
+    if request.method == 'POST':
+        form = InvoicesForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Invoice has been successfully recorded')
+        else:
+            form = InvoicesForm(request.POST)
+            messages.error(
+                request, 'Unfortunately your form submission was unsuccessful, please try again')
+    else:
+        form = InvoicesForm()
 
-	return render(request,'invoiceform.html',context={"form":form})
+    return render(request, 'invoiceform.html', context={"form": form})
+
 
 def NewSupplier(request):
-	if request.method == 'POST':
-		form = SupplierForm(request.POST)
-		if form.is_valid():
-			print("form is valid")
-			form.save()
-			messages.success(request, 'Invoice has been successfully recorded')
-		else:
-			form = SupplierForm(request.POST)
-			messages.error(request, 'Unfortunately your form submission was unsuccessful, please try again')
-	else:
-		form = SupplierForm()
-	return render(request, 'newsupplier.html', context={"form":form})
+    if request.method == 'POST':
+        form = SupplierForm(request.POST)
+        if form.is_valid():
+            print("form is valid")
+            form.save()
+            messages.success(request, 'Invoice has been successfully recorded')
+        else:
+            form = SupplierForm(request.POST)
+            messages.error(
+                request, 'Unfortunately your form submission was unsuccessful, please try again')
+    else:
+        form = SupplierForm()
+    return render(request, 'newsupplier.html', context={"form": form})
 
 
 def redir_to_invoicelist(request):
-	return redirect('invoice_list', '1')
+    return redirect('invoice_list', '1')
+
+
 def invoice_list(request, page_num):
-	NUM_ITEM_TO_SHOW = 30
+    NUM_ITEM_TO_SHOW = 30
 
-	# pageview
-	if page_num == "":
-		page_num = 1
-	page_num = int(page_num)
+    # pageview
+    if page_num == "":
+        page_num = 1
+    page_num = int(page_num)
 
-	# get all the data and arrange them in date order
-	a = Invoices.objects.all().order_by('-invoice_date')
+    # get all the data and arrange them in date order
+    a = Invoices.objects.all().order_by('-invoice_date')
 
+    # slicing the result
+    page_num -= 1
+    start_slice = page_num * NUM_ITEM_TO_SHOW
+    end_slice = start_slice + NUM_ITEM_TO_SHOW
+    page_to_show = a[start_slice:end_slice]
 
-	#slicing the result
-	page_num -= 1
-	start_slice= page_num * NUM_ITEM_TO_SHOW
-	end_slice= start_slice + NUM_ITEM_TO_SHOW
-	page_to_show = a[start_slice:end_slice]
+    if len(page_to_show) <= 0:
+        raise Http404("No result to show.")
+    # Check if there is any more result to show on the next page
+    start_slice += NUM_ITEM_TO_SHOW
+    end_slice += NUM_ITEM_TO_SHOW
+    next_page = a[start_slice:end_slice]
+    if len(next_page) > 0:
+        show_next_page = True
+    else:
+        show_next_page = False
 
-	if len(page_to_show) <= 0:
-		raise Http404("No result to show.")
-	# Check if there is any more result to show on the next page
-	start_slice += NUM_ITEM_TO_SHOW
-	end_slice += NUM_ITEM_TO_SHOW
-	next_page = a[start_slice:end_slice]
-	if len(next_page) > 0:
-		show_next_page = True
-	else:
-		show_next_page = False
+    page_num += 1
+    next_page_num = page_num + 1
+    prev_page_num = page_num - 1
 
-	page_num += 1
-	next_page_num = page_num + 1
-	prev_page_num = page_num - 1
+    # Decide to show the previous button or not
+    if prev_page_num > 0:
+        show_prev_page = True
+    else:
+        show_prev_page = False
 
-	#Decide to show the previous button or not
-	if prev_page_num > 0:
-		show_prev_page = True
-	else:
-		show_prev_page = False
+    # Calculating the total invoice value of the display page
+    costs = 0
+    for i in page_to_show:
+        costs += i.invoice_value
+    page_total = costs
 
-	# Calculating the total invoice value of the display page
-	costs = 0
-	for i in page_to_show:
-		costs += i.invoice_value
-	page_total = costs
+    if request.method == 'GET':
+        if request.user.is_authenticated():
+            if request.GET.get('filter_date'):
+                filter_date = request.GET['filter_date']
+                qyear = filter_date[:4]
+                qmonth = filter_date[5:7]
+                result = Invoices.objects.filter(
+                    invoice_date__month=qmonth, invoice_date__year=qyear).order_by('-invoice_date')
+            elif request.GET.get('payment_method'):
+                payment_method = request.GET['payment_method']
+                result = Invoices.objects.filter(
+                    payment_method=payment_method).order_by('-invoice_date')
+            elif request.GET.get('organisation'):
+                organisation = request.GET['organisation']
+                result = Invoices.objects.filter(
+                    organisation=organisation).order_by('-invoice_date')
+            else:
+                context = {
+                    'result': page_to_show,
+                    'page_total': page_total,
+                    'page_num': page_num,
+                    'next_page_num': next_page_num,
+                    'prev_page_num': prev_page_num,
+                    'show_next_page': show_next_page,
+                    'show_prev_page': show_prev_page
+                }
+                return render(request, 'invoicelist.html', context)
+        else:
+            print("user is not authenticated")
+            return render(request, 'accounts/login_error.html')
 
-	if request.method == 'GET':
-		if request.GET.get('filter_date'):
-			filter_date = request.GET['filter_date']
-			qyear = filter_date[:4]
-			qmonth = filter_date[5:7]
-			result = Invoices.objects.filter(invoice_date__month=qmonth,invoice_date__year=qyear).order_by('-invoice_date')
-		elif request.GET.get('payment_method'):
-			payment_method = request.GET['payment_method']
-			result = Invoices.objects.filter(payment_method=payment_method).order_by('-invoice_date')
-		elif request.GET.get('organisation'):
-			organisation= request.GET['organisation']
-			result = Invoices.objects.filter(organisation=organisation).order_by('-invoice_date')
-		else:
-			context = {
-				'result': page_to_show,
-				'page_total': page_total,
-				'page_num': page_num,
-				'next_page_num': next_page_num,
-				'prev_page_num': prev_page_num,
-				'show_next_page': show_next_page,
-				'show_prev_page': show_prev_page
-			}
-			return render(request, 'invoicelist.html', context)
+    return render(request, 'invoicelist.html', context={'result': result})
 
-	return render(request, 'invoicelist.html', context={'result': result})
 
 def InvoiceView(request):
-	previous_page = request.META.get('HTTP_REFERER')
-	if request.method == 'GET':
-		if request.GET.get('id'):
-			product_id = request.GET['id']
-			item = Invoices.objects.filter(id=product_id)
-	return render(request, 'invoice.html', context={'item': item, 'prev_page': previous_page})
+    previous_page = request.META.get('HTTP_REFERER')
+    if request.method == 'GET':
+        if request.GET.get('id'):
+            product_id = request.GET['id']
+            item = Invoices.objects.filter(id=product_id)
+    return render(request, 'invoice.html', context={'item': item, 'prev_page': previous_page})
 
 
 # JSON Date modules
-from django.http import JsonResponse
-import datetime
-from json import JSONEncoder
 
 
 def invoice_api(request):
-	#calculate the spending of this month
-	date = datetime.date.today()
-	this_month = date.month
-	this_year = date.year
+    # calculate the spending of this month
+    date = datetime.date.today()
+    this_month = date.month
+    this_year = date.year
 
-	this_month_invoices = Invoices.objects.filter(
-    invoice_date__month=this_month,
-    invoice_date__year=date.year,
-    organisation="Linco Care"
-	)
-	total_cost_month = 0
-	for i in this_month_invoices:
-		total_cost_month += i.invoice_value
-	total_cost_month = "%0.2f" % total_cost_month
-	# Spending of this year
-	this_year_invoices = Invoices.objects.filter(
-	invoice_date__year=date.year,
-	organisation="Linco Care"
-	)
-	total_cost_year = 0
-	for i in this_year_invoices:
-		total_cost_year += i.invoice_value
-	total_cost_year = "%0.2f" % total_cost_year
+    this_month_invoices = Invoices.objects.filter(
+        invoice_date__month=this_month,
+        invoice_date__year=date.year,
+        organisation="Linco Care"
+    )
+    total_cost_month = 0
+    for i in this_month_invoices:
+        total_cost_month += i.invoice_value
+    total_cost_month = "%0.2f" % total_cost_month
+    # Spending of this year
+    this_year_invoices = Invoices.objects.filter(
+        invoice_date__year=date.year,
+        organisation="Linco Care"
+    )
+    total_cost_year = 0
+    for i in this_year_invoices:
+        total_cost_year += i.invoice_value
+    total_cost_year = "%0.2f" % total_cost_year
 
-	# Amazon API
-	import mws, os
-	orders_api = mws.Orders(
-		access_key=os.environ['MWS_ACCESS_KEY'],
-		secret_key=os.environ['MWS_SECRET_KEY'],
-		account_id=os.environ['MWS_ACCOUNT_ID'],
-		region='UK',  # if necessary
-	)
-	service= orders_api.get_service_status()
-	last_updated_after='{}-{:02d}-01'.format(date.year,date.month)
-	response = orders_api.list_orders(marketplaceids="A1F83G8C2ARO7P", lastupdatedafter=last_updated_after, max_results='100')
-	response = response.parsed
-	total_amazon_orders = response.Orders.Order[0].OrderTotal.Amount
-	print(total_amazon_orders)
-	data = {
-		"total_cost_month": total_cost_month,
-		"total_cost_year": total_cost_year,
-		"this_month": this_month,
-		"this_year": this_year,
-		"Amazon": response,
-		"total_amazon_orders": "total_amazon_orders"
-	}
+    # Amazon API
+    import mws
+    import os
+    orders_api = mws.Orders(
+        access_key=os.environ['MWS_ACCESS_KEY'],
+        secret_key=os.environ['MWS_SECRET_KEY'],
+        account_id=os.environ['MWS_ACCOUNT_ID'],
+        region='UK',  # if necessary
+    )
+    service = orders_api.get_service_status()
+    last_updated_after = '{}-{:02d}-01'.format(date.year, date.month)
+    response = orders_api.list_orders(
+        marketplaceids="A1F83G8C2ARO7P", lastupdatedafter=last_updated_after, max_results='100')
+    response = response.parsed
+    total_amazon_orders = response.Orders.Order[0].OrderTotal.Amount
+    print(total_amazon_orders)
+    data = {
+        "total_cost_month": total_cost_month,
+        "total_cost_year": total_cost_year,
+        "this_month": this_month,
+        "this_year": this_year,
+        "Amazon": response,
+        "total_amazon_orders": "total_amazon_orders"
+    }
 
-
-	return JsonResponse(data, encoder=JSONEncoder)
+    return JsonResponse(data, encoder=JSONEncoder)
--- linco/wiki/models.py
+++ linco/wiki/models.py
@@ -8,85 +8,87 @@ from django.conf import settings
 from django.utils.timezone import now
 User = settings.AUTH_USER_MODEL
 
+
 class Category(models.Model):
-	slug = models.SlugField()
-	category_name = models.CharField(max_length=200,blank=True)
-	parent = models.ForeignKey('self', blank=True, null=True, related_name='child')
-	
-	def __unicode__(self):
-		# full_path = [self.category_name]
-		# k = self.parent
-		# while k is not None:
-		#     full_path.append(k.category_name)
-		#     k = k.parent
-		# return ' -> '.join(full_path[::-1])
-		return "%s" % self.category_name
+    slug = models.SlugField()
+    category_name = models.CharField(max_length=200, blank=True)
+    parent = models.ForeignKey(
+        'self', blank=True, null=True, related_name='child')
 
-	class Meta:
-		verbose_name_plural = 'Categories'
-		unique_together = ('slug', 'parent',) #enforcing that there can not be two categories under a parent with same slug
-		ordering = ['category_name']
+    def __unicode__(self):
+        # full_path = [self.category_name]
+        # k = self.parent
+        # while k is not None:
+        #     full_path.append(k.category_name)
+        #     k = k.parent
+        # return ' -> '.join(full_path[::-1])
+        return "%s" % self.category_name
 
-	def get_category_tree(self):
-		categorys = []
-		def parse(category):
-		    categorys.append(category)
-		    if category.parent_category:
-		        parse(category.parent_category)
-		parse(self)
-		return categorys
+    class Meta:
+        verbose_name_plural = 'Categories'
+        # enforcing that there can not be two categories under a parent with same slug
+        unique_together = ('slug', 'parent',)
+        ordering = ['category_name']
 
-	def get_sub_categorys(self):
-		categorys = []
-		all_categorys = Category.objects.all()
-        
-		def parse(category):
-		    if category not in categorys:
-		        categorys.append(category)
-		    childs = all_categorys.filter(parent_category=category)
-		    for child in childs:
-		        if category not in categorys:
-		            categorys.append(child)
-		        parse(child)
-		parse(self)
-		return categorys
+    def get_category_tree(self):
+        categorys = []
+
+        def parse(category):
+            categorys.append(category)
+            if category.parent_category:
+                parse(category.parent_category)
+        parse(self)
+        return categorys
+
+    def get_sub_categorys(self):
+        categorys = []
+        all_categorys = Category.objects.all()
+
+        def parse(category):
+            if category not in categorys:
+                categorys.append(category)
+            childs = all_categorys.filter(parent_category=category)
+            for child in childs:
+                if category not in categorys:
+                    categorys.append(child)
+                parse(child)
+        parse(self)
+        return categorys
 
 
 class Wikientry(models.Model):
-	
-	owner = models.ForeignKey(User)
-	slug = models.SlugField(unique=True)
-	title = models.CharField(max_length=200,blank=True)
-	wiki_content=models.TextField(blank=True) 
-	last_edit= models.DateTimeField(auto_now=True, auto_now_add=False)
-	created_time = models.DateTimeField(default=now)
-	category = models.ForeignKey('Category', null=True, blank=True)
-	
-	def __unicode__(self):
-		return "Subject:%s -- Content: %s" % (self.title, self.wiki_content[:20]+"...")
-	
-	def __str__(self):
-		return self.title
 
-	def get_cat_list(self):           #for now ignore this instance method,
-	    k = self.category
-	    breadcrumb = ["dummy"]
-	    while k is not None:
-	        breadcrumb.append(k.slug)
-	        k = k.parent
+    owner = models.ForeignKey(User)
+    slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=200, blank=True)
+    wiki_content = models.TextField(blank=True)
+    last_edit = models.DateTimeField(auto_now=True, auto_now_add=False)
+    created_time = models.DateTimeField(default=now)
+    category = models.ForeignKey('Category', null=True, blank=True)
 
-	    for i in range(len(breadcrumb)-1):
-	        breadcrumb[i] = '/'.join(breadcrumb[-1:i-1:-1])
-	    return breadcrumb[-1:0:-1]
+    def __unicode__(self):
+        return "Subject:%s -- Content: %s" % (self.title, self.wiki_content[:20]+"...")
 
-	class Meta:
-		ordering = ["-created_time", "-last_edit"]
-		get_latest_by = 'last_edit'
+    def __str__(self):
+        return self.title
 
-	def get_absolute_url(self):
-		return "/wiki/%s/" % self.id
+    def get_cat_list(self):  # for now ignore this instance method,
+        k = self.category
+        breadcrumb = ["dummy"]
+        while k is not None:
+            breadcrumb.append(k.slug)
+            k = k.parent
 
-	def get_category_tree(self):
-	    tree = self.category.get_category_tree()
+        for i in range(len(breadcrumb)-1):
+            breadcrumb[i] = '/'.join(breadcrumb[-1:i-1:-1])
+        return breadcrumb[-1:0:-1]
 
+    class Meta:
+        ordering = ["-created_time", "-last_edit"]
+        get_latest_by = 'last_edit'
 
+    def get_absolute_url(self):
+        return "/wiki/%s/" % self.id
+
+    def get_category_tree(self):
+        tree = self.category.get_category_tree()
